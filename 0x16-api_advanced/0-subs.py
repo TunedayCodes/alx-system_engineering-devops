0917dbb3128deabb3204f5c894fca9615bdce7e2
TunedayCodes <fadipetunde5@gmail.com>
2024-01-16 04:27:30 -0800

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-a function that queries the Reddit API and
-returns the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
-"""
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    if subreddit is None or type(subreddit) is not str:
-        return 0
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced_project'}
-    r = requests.get(base_url, headers=headers, allow_redirects=False).json()
-
-    if r.get('data') is None:
-        return 0
-
-    subscribers = r.get('data').get('subscribers')
-    return subscribers
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
